90ee2b53cdfef0e18409187df366ddc54d863d4f
Niyati Jain <Jainniyati905@gmail.com>
2026-05-07 16:30:14 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,6 @@
+name = "niyati"
+age = 20
+college = "College of computing science and information technology"
+print("your name is:", name, "Data type:", type(name))
+print("your age is:", age, "Data type:", type(age))
+print("your college name is:", college, "Data type:", type(college))
