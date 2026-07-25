126e198e7df2d0438c5579500782ff666c15e01e
Niyati Jain <Jainniyati905@gmail.com>
2026-07-25 21:39:28 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+#input function
+
+Name = input("Enter your name:")
+Age = int(input("Enter your age:"))
+print("Your cute name is:",Name)
+print("Your current age is:",Age)
+print(type(Name))
+print(type(Age))
